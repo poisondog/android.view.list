a61559a74899f81027be4a93489e2b22c4a23c9c
poisondog <poisondog@gmail.com>
2018-10-18 17:21:56 +0800

    refactor for ListAdapter

--- src/main/java/poisondog/android/view/list/ListAdapter.java
+++ src/main/java/poisondog/android/view/list/ListAdapter.java
@@ -28,6 +28,7 @@ import java.util.List;
 import poisondog.android.os.AsyncTask;
 import poisondog.android.view.list.R;
 import poisondog.android.image.ImageFetcher;
+import poisondog.core.Mission;
 
 /**
  * @author poisondog <poisondog@gmail.com>
@@ -37,6 +38,7 @@ public class ListAdapter extends BaseAdapter {
 	private List<ListItem> mItems;
 	private UpdateHandler mSubtitleHandler;
 	private ImageFetcher mFetcher;
+	private Mission<ListItem> mViewCreator;
 
 	/**
 	 * Constructor
@@ -45,6 +47,7 @@ public class ListAdapter extends BaseAdapter {
 		super();
 		mContext = context;
 		mItems = new ArrayList<ListItem>();
+		mViewCreator = new ListItemViewCreator();
 
 		try {
 			mFetcher = new ImageFetcher(mContext, 500, 500, mContext.getExternalCacheDir().getPath() + "/");
@@ -101,9 +104,12 @@ public class ListAdapter extends BaseAdapter {
 
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
-		ListItemView row = new ListItemView(mContext);
-		update(row, getItem(position));
-		return row;
+		try {
+			return (View) mViewCreator.execute(getItem(position));
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		return null;
 	}
 
 	public TextView getSubtitle(View row) {
@@ -114,46 +120,73 @@ public class ListAdapter extends BaseAdapter {
 		return (TextView) row.findViewById(R.id.comment);
 	}
 
+	@Override
+	public boolean isEmpty() {
+		return mItems.isEmpty();
+	}
+
+	public void setSubtitleHandler(UpdateHandler handler) {
+		mSubtitleHandler = handler;
+	}
+
+	class ListItemViewCreator implements Mission<ListItem> {
+		@Override
+		public ListItemView execute(ListItem item) {
+			ListItemView row = new ListItemView(mContext);
+			update(row, item);
+			return row;
+		}
+	}
+
 	private void update(final ListItemView row, final ListItem obj) {
-		AsyncTask<String, Void, String> updateTitle = new AsyncTask<String, Void, String>() {
-			@Override
-			protected String doInBackground(String... none) {
-				return obj.getTitle();
-			}
-			@Override
-			protected void onPostExecute(String result) {
-				row.getTitle().setText(result);
-			}
-		};
-		updateTitle.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, "");
+		row.getTitle().setText(obj.getTitle());
+//		AsyncTask<String, Void, String> updateTitle = new AsyncTask<String, Void, String>() {
+//			@Override
+//			protected String doInBackground(String... none) {
+//				return obj.getTitle();
+//			}
+//			@Override
+//			protected void onPostExecute(String result) {
+//				row.getTitle().setText(result);
+//			}
+//		};
+//		updateTitle.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, "");
 
-		AsyncTask<String, Void, String> updateSubtitle = new AsyncTask<String, Void, String>() {
-			@Override
-			protected String doInBackground(String... none) {
-				return obj.getSubtitle();
-			}
-			@Override
-			protected void onPostExecute(String result) {
-				if (result == null)
-					row.getSubtitle().setVisibility(View.GONE);
-				row.getSubtitle().setText(result);
-			}
-		};
-		updateSubtitle.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, "");
+		if (obj.getSubtitle() == null)
+			row.getSubtitle().setVisibility(View.GONE);
+		else
+			row.getSubtitle().setText(obj.getSubtitle());
+//		AsyncTask<String, Void, String> updateSubtitle = new AsyncTask<String, Void, String>() {
+//			@Override
+//			protected String doInBackground(String... none) {
+//				return obj.getSubtitle();
+//			}
+//			@Override
+//			protected void onPostExecute(String result) {
+//				if (result == null)
+//					row.getSubtitle().setVisibility(View.GONE);
+//				row.getSubtitle().setText(result);
+//			}
+//		};
+//		updateSubtitle.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, "");
 
-		AsyncTask<String, Void, String> updateComment = new AsyncTask<String, Void, String>() {
-			@Override
-			protected String doInBackground(String... none) {
-				return obj.getComment();
-			}
-			@Override
-			protected void onPostExecute(String result) {
-				if (result == null)
-					row.getComment().setVisibility(View.GONE);
-				row.getComment().setText(result);
-			}
-		};
-		updateComment.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, "");
+		if (obj.getComment() == null)
+			row.getComment().setVisibility(View.GONE);
+		else
+			row.getComment().setText(obj.getComment());
+//		AsyncTask<String, Void, String> updateComment = new AsyncTask<String, Void, String>() {
+//			@Override
+//			protected String doInBackground(String... none) {
+//				return obj.getComment();
+//			}
+//			@Override
+//			protected void onPostExecute(String result) {
+//				if (result == null)
+//					row.getComment().setVisibility(View.GONE);
+//				row.getComment().setText(result);
+//			}
+//		};
+//		updateComment.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, "");
 
 		if (obj.getImage() == null && obj.getDefaultImage() <= 0) {
 			row.getImage().setVisibility(View.GONE);
@@ -172,12 +205,4 @@ public class ListAdapter extends BaseAdapter {
 			mFetcher.loadImage(obj.getState(), row.getState());
 	}
 
-	@Override
-	public boolean isEmpty() {
-		return mItems.isEmpty();
-	}
-
-	public void setSubtitleHandler(UpdateHandler handler) {
-		mSubtitleHandler = handler;
-	}
 }
