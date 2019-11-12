0aa98ca1f16d22412521a7a4e3a845469e8d6cd8
poisondog <poisondog@gmail.com>
2019-11-12 12:01:50 +0800

    update for MainActivity

--- app/src/main/java/poisondog/android/view/list/app/MainActivity.java
+++ app/src/main/java/poisondog/android/view/list/app/MainActivity.java
@@ -1,7 +1,6 @@
 package poisondog.android.view.list.app;
 
 import android.app.Activity;
-import android.content.DialogInterface;
 import android.os.Bundle;
 import android.os.Environment;
 import android.support.v7.widget.GridLayoutManager;
@@ -14,7 +13,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import poisondog.android.view.EntityView;
-import poisondog.android.view.FileView;
 import poisondog.android.view.list.app.R;
 import poisondog.android.view.list.DataItem;
 import poisondog.android.view.list.ItemView;
@@ -32,9 +30,7 @@ import poisondog.vfs.IFile;
 import poisondog.vfs.IFolder;
 
 public class MainActivity extends Activity {
-	private DialogInterface.OnClickListener mListener;
 	private EntityView mRoot;
-	private List<IFile> mContent;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -45,73 +41,49 @@ public class MainActivity extends Activity {
 		LayoutAnimationController animation = AnimationUtils.loadLayoutAnimation(this, resId);
 
 		LinearLayout root = (LinearLayout) findViewById(R.id.root);
-//		mRoot = new FileView(this);
 		mRoot = new EntityView(this);
 		mRoot.getRecyclerView().setLayoutAnimation(animation);
 		mRoot.setLayoutManager(new LinearLayoutManager(this));
 		mRoot.setRefreshHandler(new RefreshData());
-		String download = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).getPath() + "/";
-		try {
-			IFolder mFolder = (IFolder)FileFactory.getFile(download);
-			FileFilter filter = new FileFilter();
-			filter.setIncludeRule(new OnlyImage());
-			mContent = new ArrayList<IFile>(filter.execute(mFolder.getChildren()));
-			mRoot.refresh();
-			mRoot.setOnClickListener(new View.OnClickListener() {
-				@Override
-				public void onClick(View v) {
-					ItemView target = (ItemView) v;
-					try {
-						System.out.println(URLUtils.file(((IData)target.getItem().getData()).getUrl()));
-					} catch(Exception e) {
+
+		mRoot.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				ItemView target = (ItemView) v;
+				try {
+					System.out.println(URLUtils.file(((IData)target.getItem().getData()).getUrl()));
+				} catch(Exception e) {
+				}
+			}
+		});
+
+		mRoot.setOnLongClickListener(new View.OnLongClickListener() {
+			@Override
+			public boolean onLongClick(View v) {
+				GridLayoutManager layoutManager = new GridLayoutManager(MainActivity.this, 2);
+				layoutManager.setSpanSizeLookup(new GridLayoutManager.SpanSizeLookup() {
+					@Override
+					public int getSpanSize(int position) {
+						if (mRoot.getItemViewLayout(position) == R.layout.header_item)
+							return 2;
+						return 1;
+					}
+				});
+				mRoot.setLayoutManager(layoutManager);
+				for (DataItem item : mRoot.getItems()) {
+					if (item.getLayout() == R.layout.list_item) {
+						item.setLayout(R.layout.grid_item);
 					}
 				}
-			});
-			mRoot.setOnLongClickListener(new View.OnLongClickListener() {
-				@Override
-				public boolean onLongClick(View v) {
+				return true;
+			}
+		});
 
-					GridLayoutManager layoutManager = new GridLayoutManager(MainActivity.this, 2);
-					layoutManager.setSpanSizeLookup(new GridLayoutManager.SpanSizeLookup() {
-						@Override
-						public int getSpanSize(int position) {
-							if (mRoot.getItemViewLayout(position) == R.layout.header_item)
-								return 2;
-							return 1;
-						}
-					});
-					mRoot.setLayoutManager(layoutManager);
-
-//					GridCreator mCreator = new GridCreator();
-//					ArrayList<DataItem> result = new ArrayList<DataItem>();
-//					result.add(SimpleItem.header("Head", "second", "third"));
-//					for (IFile f : mContent) {
-//						try {
-//							result.add(mCreator.execute(f));
-//						} catch(Exception e) {
-//						}
-//						if (Math.random() < 0.1)
-//							result.add(SimpleItem.header("first", "second", "third"));
-//					}
-//					mRoot.setItems(result);
-
-					for (DataItem item : mRoot.getItems()) {
-						if (item.getLayout() == R.layout.list_item) {
-							item.setLayout(R.layout.grid_item);
-						}
-					}
-
-					return true;
-				}
-			});
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-//		mRoot.setEmpty(true);
+		mRoot.refresh();
 		root.addView(mRoot);
 	}
 
-	class PhotoCreator implements Mission<IFile> {
+	class ListItemFactory implements Mission<IFile> {
 		@Override
 		public DataItem execute(IFile f) throws Exception {
 			IData data = (IData)f;
@@ -126,36 +98,34 @@ public class MainActivity extends Activity {
 		}
 	}
 
-	class GridCreator implements Mission<IFile> {
-		@Override
-		public DataItem execute(IFile f) throws Exception {
-			IData data = (IData)f;
-			String filename = URLUtils.file(f.getUrl());
-			String time = TimeFormatUtils.toString(f.getLastModifiedTime());
-			String size = SizeFormatUtils.toString(data.getSize());
-			SimpleItem item = SimpleItem.layout(filename, time, size, R.layout.grid_item);
-			item.setDefaultImage(R.drawable.file_txt);
-			item.setData(data);
-			item.setImage(data.getUrl());
-			return item;
-		}
-	}
-
 	class RefreshData implements Runnable {
-		private PhotoCreator mCreator;
+		private ListItemFactory mCreator;
 		/**
 		 * Constructor
 		 */
 		public RefreshData() {
-			mCreator = new PhotoCreator();
+			mCreator = new ListItemFactory();
 		}
 		@Override
 		public void run() {
 			mRoot.setLoading(true);
-			Collections.sort(mContent, new NameOrder());
+
+			mRoot.setLayoutManager(new LinearLayoutManager(MainActivity.this));
+			List<IFile> content = new ArrayList<IFile>();
+			String download = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).getPath() + "/";
+			try {
+				IFolder mFolder = (IFolder)FileFactory.getFile(download);
+				FileFilter filter = new FileFilter();
+				filter.setIncludeRule(new OnlyImage());
+				content = new ArrayList<IFile>(filter.execute(mFolder.getChildren()));
+			} catch(Exception e) {
+				e.printStackTrace();
+			}
+
+			Collections.sort(content, new NameOrder());
 			ArrayList<DataItem> result = new ArrayList<DataItem>();
 			result.add(SimpleItem.header("Head", "second", "third"));
-			for (IFile f : mContent) {
+			for (IFile f : content) {
 				try {
 					result.add(mCreator.execute(f));
 				} catch(Exception e) {
